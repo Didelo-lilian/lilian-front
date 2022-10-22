e8e31f075628fa788e276cf6876880069d196e7f
SOLER Lilian <soler.lilian64@gmail.com>
2022-10-22 13:20:59 +0200

    Integration new api home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,64 @@
 <template>
-	<div class="home">
-		<main>
+  <div class="home">
+    <main>
       <div v-for="article in articles" :key="article.language">
         <article>
           <p v-for="(paragraph, index) in article.paragraphs" :key=index>
             {{ paragraph }}
           </p>
         </article>
-        <hr />
+        <hr/>
       </div>
-		</main>
+    </main>
   </div>
 
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { AxiosResponse } from "axios";
+import {defineComponent} from "vue";
+import {AxiosResponse} from "axios";
 import _axios from "@/plugins/axios";
 
 interface Article {
-	language: string;
+  language: string;
   paragraphs: string[];
 }
 
 interface DataComponent {
-	articles: null | Article[];
-  languages: null | string[];
+  articles: null | Article[];
+  priorityLanguages: null | string[];
+  nonPriorityLanguages: null | string[];
+}
+
+interface ResponseAux {
+  noLanguage: number;
+  paragraphs: string[];
 }
 
 
-
 export default defineComponent({
-	name: "Home",
-	data(): DataComponent {
-		return {
-			articles: [],
-      languages: ["French", "English", "Spanish"]
-		};
-	},
+  name: "Home",
+  data(): DataComponent {
+    return {
+      articles: [],
+      priorityLanguages: ["French", "English"],
+      nonPriorityLanguages: ["Spanish", "German"]
+    };
+  },
   methods: {
     async getArticles(languages: string[]) {
       for (const language of languages) {
         await _axios
             .get(`v2/home/${language}`)
-            .then((response: AxiosResponse) => {
-              let paragraphs : string[] = [];
-              response.data.forEach((p: {paragraph: string}) => {
-                paragraphs?.push(p.paragraph);
-              });
-              this.articles?.push({'language': language, "paragraphs": paragraphs});
+            .then((response: AxiosResponse<ResponseAux[]>) => {
+              const articles: Article[] = [];
+              for (const article of response.data) {
+                articles.push({
+                  language: language,
+                  paragraphs: article.paragraphs
+                });
+              }
+              this.articles = this.articles.concat(articles);
             })
             .catch((error) => {
               console.log(error);
@@ -57,41 +66,49 @@ export default defineComponent({
       }
     }
   },
-	mounted() {
-    if(this.languages) {
-      this.getArticles(this.languages);
+  mounted() {
+    if (this.priorityLanguages) {
+      this.getArticles(this.priorityLanguages);
     }
-	},
+    if (this.nonPriorityLanguages) {
+      setTimeout(() => {
+        this.getArticles(this.nonPriorityLanguages);
+      }, 1000);
+    }
+  },
 });
 </script>
 
 <style scoped lang="scss">
 .home {
-	main {
-		width: 80vw;
-		margin-top: 10vh;
-		margin-bottom: 10vw;
+  main {
+    width: 80vw;
+    margin-top: 10vh;
+    margin-bottom: 10vw;
 
-		div {
-			margin: 0 2.5vw;
-			article p {
-				font-size: 1.5rem;
-				text-align: justify;
-				width: 70vw;
-				margin-left: 2.5vw;
-				:first-child {
-					font-size: 2rem;
-					font-weight: bold;
-					text-align: center;
-				}
-			}
-		}
-	}
-	hr {
-		display: block;
-		border: 1px solid white;
-		width: 40vw;
-		text-align: center;
-	}
+    div {
+      margin: 0 2.5vw;
+
+      article p {
+        font-size: 1.5rem;
+        text-align: justify;
+        width: 70vw;
+        margin-left: 2.5vw;
+
+        :first-child {
+          font-size: 2rem;
+          font-weight: bold;
+          text-align: center;
+        }
+      }
+    }
+  }
+
+  hr {
+    display: block;
+    border: 1px solid white;
+    width: 40vw;
+    text-align: center;
+  }
 }
 </style>
